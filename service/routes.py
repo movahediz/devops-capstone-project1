5884af631565d08239c2aaf47d47268333e5ec76
movahediz <zahra.movahhedi@gmail.com>
2023-07-07 00:21:22 +0200

    recommit the flinkt

--- service/routes.py
+++ service/routes.py
@@ -156,4 +156,4 @@ def delete_accounts(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-    return "", status.HTTP_204_NO_CONTENT
+    return "", status.HTTP_204_NO_CONTENT 
